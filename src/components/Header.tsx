d5f4584ebcff924d9c6347b952a24ac385ff2d75
Kamalfeegar <103608947+Kamalfeegar@users.noreply.github.com>
2023-03-19 20:28:40 +0100

    Headern klar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,23 @@
 import React from 'react'
 import './Header.css'
 import CleaningServicesIcon from '@mui/icons-material/CleaningServices';
+import { Link } from 'react-router-dom';
 
 
 function Header() {
   return (
     <div className='headern'>
-        <span><h1>Städa fint</h1><CleaningServicesIcon className='CleaningServicesIcon'/></span>
-        <h2>TJÄNSTER</h2>
-        <h2>OM OSS</h2>
-        <h2>KONTAKT</h2>
+        
+        <ul className='header_ul'>
+        <img src="https://img.icons8.com/external-bearicons-detailed-outline-bearicons/64/null/external-Clean-House-chinese-new-year-bearicons-detailed-outline-bearicons.png"/>
+        <li className='header_li'>TJÄNSTER</li>
+        <li className='header_li'>OM OSS</li>
+        <li className='header_li'>KONTAKT</li>
+        <li className='header_li'>
+        <Link to="/bookingpage"><button className='btn'>FÖRETAG</button> </Link>
+        </li>
+        
+        </ul>
 
       
     </div>
