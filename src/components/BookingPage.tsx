8c42b5d3521ddac03d51b3ca208b4273cc96eab3
Mohamed Yusuf <momme40@hotmail.com>
2023-04-02 14:14:24 +0200

    done

--- src/components/BookingPage.tsx
+++ src/components/BookingPage.tsx
@@ -1,21 +1,129 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import FetchAll from '../fetchApi/FetchAll'
-
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import FetchAll from "../fetchApi/FetchAll";
+import axios from "axios";
+import IBooking from "../fetchApi/Interface";
 
 
 function BookingsPage() {
-    return (
-      <div>
-        <h1>Dina bokade städningar</h1>
-        <p>Här kan du se en lista över dina bokade städningar:</p>
-        <FetchAll></FetchAll>
-        <ul>
-          
-        </ul>
-        <Link to="/">Tillbaka till startsidan</Link>
-      </div>
-    );
-  }
+  const [level, setLevel] = useState("");
+  const [städare, setStädare] = useState("");
+  const [date, setDate] = useState("");
+  const [time, setTime] = useState("");
+  const [bookings, setBookings] = useState<IBooking[]>([]);
+  
 
-  export default BookingsPage
+
+
+  
+
+  const postBookings = async () => {
+    const response = await axios.post("http://localhost:3000/clean", {
+      Level: level,
+      Städare: städare,
+      Status: false,
+      Date: date,
+      Time: time,
+    });
+    const newBooking = response.data as IBooking;
+    setBookings([...bookings, newBooking]);
+    setStädare("");
+    setDate("");
+    setTime("");
+    setLevel("");
+  };
+
+  useEffect(() => {
+    const fetchBookings = async () => {
+      const response = await axios.get("http://localhost:3000/clean");
+      setBookings(response.data);
+      console.log(response.data);
+    };
+    fetchBookings();
+  }, []);
+
+  return (
+    <div>
+      <h1>Dina bokade städningar</h1>
+
+      
+  <select id="names" name="names" value={städare} onChange={(e) => setStädare(e.target.value)}>
+  <option value="John">John</option>
+  <option value="Mary">Mary</option>
+  <option value="Peter">Peter</option>
+  <option value="Lisa">Lisa</option>
+</select>
+
+
+      
+      
+      <input
+        value={date}
+        type="date"
+        name="birth_date"
+        onChange={(e) => setDate(e.target.value)}
+      ></input>
+      <input
+        value={time}
+        type="time"
+        name="start_time"
+        onChange={(e) => setTime(e.target.value)}
+      ></input>
+      <br></br>
+      <input
+        type="radio"
+        name="Topp"
+        id="Topp"
+        value="Topp"
+        checked={level === "Topp"}
+        onChange={(e) => setLevel(e.target.value)}
+      ></input>
+      <label htmlFor="Topp">Topp</label>
+      <br></br>
+      <input
+        type="radio"
+        name="Basic"
+        id="Basic"
+        value="Basic"
+        checked={level === "Basic"}
+        onChange={(e) => setLevel(e.target.value)}
+      ></input>
+      <label htmlFor="Basic">Basic</label>
+      <br></br>
+      <input
+        type="radio"
+        name="Diamant"
+        id="Diamant"
+        value="Diamant"
+        checked={level === "Diamant"}
+        onChange={(e) => setLevel(e.target.value)}
+      ></input>
+      <label htmlFor="Diamant">Diamant</label>
+      <br></br>
+      <input
+        type="radio"
+        name="Fönster"
+        id="Fönster"
+        checked={level === "Fönster"}
+        value="Fönster"
+        onChange={(e) => setLevel(e.target.value)}
+      ></input>
+      <label htmlFor="Fönster">Fönster</label>
+      <br></br>
+      <button id="btn-1" onClick={() => postBookings()}>
+        Boka nu
+      </button>
+
+      <p>Här kan du se en lista över dina bokade städningar:</p>
+      <FetchAll bookings={bookings} setBookings={setBookings}></FetchAll>
+      <ul></ul>
+
+      <h1>Utförda bokningar</h1>
+      <p>Här kan du se en lista över dina Utförda bokade städningar</p>
+
+      <Link to="/">Tillbaka till startsidan</Link>
+    </div>
+  );
+}
+
+export default BookingsPage;
--- src/fetchApi/FetchAll.tsx
+++ src/fetchApi/FetchAll.tsx
@@ -1,52 +1,52 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import '../fetchApi/FetchAll.css'
-import  IBooking  from '../fetchApi/Interface';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "../fetchApi/FetchAll.css";
+import IBooking from "../fetchApi/Interface";
+
+interface propsType {
+  bookings: IBooking[];
+  setBookings: (bookings: IBooking[]) => void;
+}
 
 
+const Booking = (props: propsType) => {
+  console.log(props);
+  const bookings = props.bookings;
 
-const Booking = () => {
-  const [bookings, setBookings] = useState<IBooking[]>([]);
-
-  useEffect(() => {
-    const fetchBookings = async () => {
-      const response = await axios.get('http://localhost:3000/clean');
-      setBookings(response.data);
-      console.log(response.data);
-    };
-    fetchBookings();
-  }, []);
   
+
   const handleDelete = async (id: number) => {
     try {
       await axios.delete(`http://localhost:3000/clean/${id}`);
-      setBookings((bookings) => bookings.filter((booking) => booking._id !== id));
+      const newBookings = bookings.filter(
+        (booking: IBooking) => booking._id !== id
+      );
+      props.setBookings(newBookings);
     } catch (error) {
       console.error(error);
     }
   };
-  const deleteOne = async (id: number) => {
-    await handleDelete( id );
-    const updatedBookings = bookings.filter(booking => booking._id !== id);
-    setBookings(updatedBookings);
-  }
-  
 
   return (
-    <div className='div'>
-      <ul className='ul'>
-        {bookings.map((booking) => (
-          <li className='li' key={booking.uuid} >
-            <h2 className='h2'>{booking.Time}</h2>
-            <h2 className='h2'>{booking.Level}</h2>
-            <h2 className='h2'>{booking.Status}</h2>
-            <h2 className='h2'>{booking.Städare}</h2>
-            <button onClick={() => deleteOne(booking._id)}>Delete</button>
+    <div className="div">
+      <ul className="ul">
+        {bookings.map((booking: IBooking) => (
+          <li className="li" key={booking.uuid}>
+            <h2 className="h2">{booking.Time}</h2>
+            <h2 className="h2">
+              {new Date(booking.Date.toString()).toISOString().split("T")[0]}
+            </h2>
+            <p className="h2">{booking.Level}</p>
+            <h2 className="h2">{booking.Status}</h2>
+            <h2 className="h2">{booking.Städare}</h2>
+            <button onClick={() => handleDelete(booking._id)}>Delete</button>
           </li>
-        )) }
+        ))}
       </ul>
-    </div> 
+    </div>
   );
-}
+};
 
 export default Booking;
+
+
